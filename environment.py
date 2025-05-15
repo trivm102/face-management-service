eda41f3e440f93b93b67703975e1e9aa698e8c62
Vo Minh Tri <vo.minh-tri1@hdsaison.com.vn>
2025-05-15 11:20:21 +0700

    change environment

--- environment.py
+++ environment.py
@@ -4,7 +4,7 @@ API_KEY = os.getenv("API_KEY")
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
-AWS_BUCKET_NAME="mysearchface"
-AWS_S3_FOLDER="uploads"
+AWS_BUCKET_NAME=os.getenv("AWS_BUCKET_NAME")
+AWS_S3_FOLDER=os.getenv("AWS_S3_FOLDER")
  
 ENV = os.getenv("ENV", "development")
